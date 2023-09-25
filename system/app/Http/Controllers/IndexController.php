18befbe37db753621338f6ac3522280f5e78eb40
Sunardi <71060025+naldy-dy@users.noreply.github.com>
2023-09-25 16:12:23 +0700

    sadad

--- system/app/Http/Controllers/IndexController.php
+++ system/app/Http/Controllers/IndexController.php
@@ -17,6 +17,43 @@ class IndexController extends Controller
         $order->notlp = request('notlp');
         $order->qty = request('qty');
         $order->total_price = request('total_price');
-        $order->status = "Unpied;
+        $order->status = "Unpied";
+
+        /*Install Midtrans PHP Library (https://github.com/Midtrans/midtrans-php)
+composer require midtrans/midtrans-php
+                              
+Alternatively, if you are not using **Composer**, you can download midtrans-php library 
+(https://github.com/Midtrans/midtrans-php/archive/master.zip), and then require 
+the file manually.   
+
+require_once dirname(__FILE__) . '/pathofproject/Midtrans.php'; */
+
+//SAMPLE REQUEST START HERE
+
+// Set your Merchant Server Key
+\Midtrans\Config::$serverKey = 'YOUR_SERVER_KEY';
+// Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+\Midtrans\Config::$isProduction = false;
+// Set sanitization on (default)
+\Midtrans\Config::$isSanitized = true;
+// Set 3DS transaction for credit card to true
+\Midtrans\Config::$is3ds = true;
+
+$params = array(
+    'transaction_details' => array(
+        'order_id' => rand(),
+        'gross_amount' => 10000,
+    ),
+    'customer_details' => array(
+        'first_name' => 'budi',
+        'last_name' => 'pratama',
+        'email' => 'budi.pra@example.com',
+        'phone' => '08111222333',
+    ),
+);
+
+$snapToken = \Midtrans\Snap::getSnapToken($params);
+
+
     }
 }
